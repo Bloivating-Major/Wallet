1c7959f58564336a2497fe7742b2f87ff72029d9
Sambhav <sambhav12wakhariya@gmail.com>
2025-12-15 13:47:45 +0530

    feat: created transaction route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json());
+
 async function initDB() {
     try {
         await sql`CREATE TABLE IF NOT EXISTS transactions(
@@ -26,6 +28,28 @@ async function initDB() {
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+})
+
+app.post("/api/transactions", async (req, res)=>{
+    try {
+        const {title, amount, category, user_id} = req.body;
+        if(!title || !user_id || !category || !amount === undefined){
+            return res.status(400).json({message : "All fields are required"});
+        }
+
+        const transaction = await sql`
+            INSERT INTO transactions(user_id, title, amount, category)
+            VALUES (${user_id}, ${title}, ${amount}, ${category})
+            RETURNING *
+        `;
+
+        console.log("Transaction created successfully", transaction);
+        res.status(201).json(transaction[0]);
+
+    } catch (error) {
+        console.log("Error creating the transaction", error);
+        res.status(500).json({message : "Internal Server Error"});
+    }
 })
 
 initDB().then(() => {
